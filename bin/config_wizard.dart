a1de4e307c1539ed3232172bf198ec173f6335b8
Nazib Al Alam <nazibalalam@gmail.com>
2022-04-25 23:34:50 +0600

    updated

---
+++ bin/config_wizard.dart
@@ -0,0 +1,5 @@
+class ConfigWizard {
+  void _startWizard() {
+    // TODO: implement
+  }
+}
--- bin/salat_cli.dart
+++ bin/salat_cli.dart
@@ -1,13 +1,18 @@
 import 'package:adhan_dart/adhan_dart.dart';
 import 'package:intl/intl.dart';
 import 'package:args/args.dart';
+import 'config_wizard.dart';
 
 void main(List<String> arguments) {
-  // Parse the command line arguments
   final parser = ArgParser();
   parser.addOption('latitude', abbr: 'l', defaultsTo: '0.0');
   parser.addOption('longitude', abbr: 'g', defaultsTo: '0.0');
   parser.addFlag('help', abbr: 'h', defaultsTo: false);
+  parser.addFlag('configure', abbr: 'c', defaultsTo: false);
+
+  if (arguments.contains('--configure')) {
+    _startWizard();
+  }
 
   final myCoordinates = Coordinates(23.9088, 89.1220);
   final date = DateTime.now();
