b811d533b83f9d295c3bf1cc3cb42b5ccfa0287a
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2022-04-26 14:10:46 +0600

    CLI Dialog Package is only for Windows, so switched to a non-null-safe package!

--- lib/salat_cli.dart
+++ lib/salat_cli.dart
@@ -2,38 +2,62 @@ import 'package:adhan_dart/adhan_dart.dart';
 import 'package:chalk/chalk.dart';
 import 'package:intl/intl.dart';
 import 'package:args/args.dart';
-import 'package:cli_dialog/cli_dialog.dart';
+import 'package:dart_inquirer/dart_inquirer.dart';
 import 'Client.dart';
 
-String askForMissingArgs() {
-  final dialog = CLI_Dialog(listQuestions: [
-    [
-      {
-        "question": "What you want to do?",
-        "options": [
-          "See today's Salah timimgs",
-          "Configure Salah Daemon Settings",
-          "Help",
-          "Quit"
-        ],
-      },
-      "action"
-    ]
-  ]);
+Future askForMissingArgs() async {
+  // final dialog = CLI_Dialog(listQuestions: [
+  //   [
+  //     {
+  //       "question": "What you want to do?",
+  //       "options": [
+  //         "See today's Salah timimgs",
+  //         "Configure Salah Daemon Settings",
+  //         "Help",
+  //         "Quit"
+  //       ],
+  //     },
+  //     "action"
+  //   ]
+  // ]);
 
-  final result = dialog.ask();
+  // final result = dialog.ask();
 
-  return result["action"];
+  // return result["action"];
+
+  List<Question> questions = [
+    ListQuestion("action", "What you want to do?", [
+      "See today's Salah timimgs",
+      "Configure Salah Daemon Settings",
+      "Help",
+      "Quit"
+    ])
+  ];
+
+  Prompt prompt = Prompt(questions);
+
+  String action = (await prompt.execute())["action"];
+
+  return action;
 }
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   print(
     chalk.blue("As-salamu alaykum wa Ra'hmatullaahi wa Barakaatuhu!\n").bold(),
   );
 
-  final action = askForMissingArgs();
+  final parser = ArgParser();
+  parser.addFlag("configure", abbr: "c", help: "Configure Salah Daemon");
+  parser.addFlag("help", abbr: "h", help: "Help");
+  parser.addFlag("today", abbr: "t", help: "See today's Salah timimgs");
 
-  print(action);
+  // print(arguments);
+
+  if (arguments.isEmpty) {
+    final action = await askForMissingArgs();
+
+    print(action);
+  }
 
   // final parser = ArgParser();
   // parser.addOption('latitude', abbr: 'l', defaultsTo: '0.0');
